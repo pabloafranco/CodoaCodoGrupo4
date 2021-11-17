ea9778d50247c5b69d1580ef583abb7664acfdfe
Pablo Franco <pabloafranco@hotmail.com>
2021-11-17 13:13:23 -0300

    1er commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import util
+from util.funciones import *
+
+# caso 1
+print ("Caso 1")
+print (reemplazar('Esto es  una prueba', ' ', '-'))
+
+# caso 2
+print ("Caso 2")
+print(mayPorMin("Esto cambia Mayusculas por minusculas y viceversa"))
+
+# caso 3
+print ("Caso 3")
+print(cambioLetra('Cambiar una Letra', 12, 'l'))
+
+# caso 4
+print ("Caso 4")
+print(primeraLetraMay("veamos esta prueba si funciona bien"))
+
+#caso 5
+print ("Caso 5")
+print (subCampeon([3,4,42,1,4,621,621,620,3,5]))
+
+
+#caso 5a
+print ("Caso 5a")
+impTriangulo(13)
+
+#caso 5b
+caracterMasUsado("Codo a codo", True)
