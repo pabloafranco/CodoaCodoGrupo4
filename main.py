eed91849f17ddceb371b77ad927df6cb04acef5b
Pablo Franco <pabloafranco@hotmail.com>
2021-11-22 18:50:35 -0300

    4to commit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import util
 from util.funciones import *
+import util
+from util.complejos import *
+import util
+from util.vectores import *
+
 
 ans=True
 while ans:
@@ -11,7 +16,15 @@ while ans:
     5.Devuelve la puntuacion del subcampeon,([3,4,42,1,4,621,621,620,3,5])
     6.(5a)Recibe un numero entero y imprima  un triangulo de numeros de altura. (impTriangulo(13))
     7.(5b)reciba string, devolver 3 caracteres mas usados en orden descendiente.(caracterMasUsado("Codo a codo", True))
-    8.Exit/Quit
+    8.Sumar complejos (20,1) + (11,45)
+    9.Restar complejos (10,18) + (8,45)
+    10.Multiplicar complejos (2,4) * (3,-2)
+    11.Sumar dos vectores A = (2;1;−2) B = (1;-3;1) 
+    12.Restar dos vectores A = (4;-8;6) B = (1;4;2) 
+    13.Multiplicar dos vectores A = (1;2;3) B = (4;5;6) 
+    14.Dividir vector por escalar A = (4;6;8) escalar 2
+    15.Matriz (3x3) + Vector
+    99.Exit/Quit
     """)
     ans=input("Que quiere hacer? ")
     if ans=="1":
@@ -52,7 +65,44 @@ while ans:
         else:
             lnocase=False
         caracterMasUsado(texto, lnocase)       
-    elif ans=="8":
+    elif ans=="8":        
+        comple1 = Complejo(20,1) 
+        comple2 = Complejo(11,45)
+        print (Complejo.sumar(comple1,comple2))
+
+    elif ans=="9":        
+        comple1 = Complejo(10,18)  
+        comple2 = Complejo(8,45)
+
+        print (Complejo.restar(comple1,comple2))
+
+    elif ans=="10":
+        comple1 = Complejo(2,4)
+        comple2 = Complejo(3,-2)
+        print (Complejo.multiplicar(comple1,comple2))
+
+    elif ans=="11":
+        a = Vector(2,1,-2)
+        b = Vector(1,-3,1) 
+        print (Vector.sumar(a,b))
+
+    elif ans=="12":
+        a = Vector(4,-8,6)
+        b = Vector(1,4,2) 
+        print (Vector.restar(a,b))
+
+    elif ans=="13":
+        a = Vector(1,2,3)
+        b = Vector(4,5,6) 
+        print (Vector.multiplicar(a,b))
+
+    elif ans=="14":
+        a = Vector(4,6,8)
+        escalar = 2
+        print (Vector.dividir(a,escalar))
+
+
+    elif ans=="99":
        print("\n Adios") 
        ans = None
     else:
